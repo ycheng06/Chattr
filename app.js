e7167359275dec892b121998754f81af8de31992
Jason Cheng <yannru.cheng@gmail.com>
2015-02-18 20:55:53 +0800

    basic setup for a chat room. can now chat with multiple parties.

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var http = require('http');
+var socket = require('socket.io');
+var app = express();
+
+var server = http.createServer(app);
+var io = socket.listen(server);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', function(client){
+	console.log("Client connected...");
+	client.on('join', function(name){
+		client.nickname = name;
+	});
+
+	client.on('messages', function(message){
+		//get nickname of this client
+		var nickname = client.nickname;
+		console.log(message);
+
+		//broad cast message to all client in the chat
+		client.broadcast.emit('messages', nickname + ": " + message);
+
+		//send the same message back to our client
+		client.emit('messages', nickname + ": " + message);
+	});
+});
+
+
+server.listen(8080);
